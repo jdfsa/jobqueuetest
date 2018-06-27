fe7801aa0b0578a0646b409dbcfe2bdf41a8391d
jacques.sa@outlook.com <jacques.sa@outlook.com>
2018-06-27 20:06:39 -0300

    using json/read native api to read stream content

--- src/job_queue/core.clj
+++ src/job_queue/core.clj
@@ -1,32 +1,11 @@
 (ns job-queue.core
   (:require [clojure.data.json :as json]))
 
-(def prevcontent (atom ""))
-(def content (atom ""))
+(defn read-json-content []
+  (json/read (java.io.BufferedReader. (java.io.InputStreamReader. System/in))))
 
-(defn readcontent 
-  "Read in content"
-  [& args]
-  
-  (println "Import jobs")
-
-  (doseq [ln (line-seq (java.io.BufferedReader. *in*))]
-    (reset! content ln))
-)
-
-(defn writecontent
-  [& args]
-  (loop [a []]
-    (Thread/sleep 1000)
-    (when-not (= @content @prevcontent)
-      (reset! prevcontent @content)
-      (println "PRINTANDO A PARADA" @content))
-    (recur a))
-  )
-
-(defn -main []
-
-  (.start (Thread. (fn [] (readcontent))))
-    
-  (.start (Thread. (fn [] (writecontent))))
-)
+(defn -main [& args]
+  (println "IMPORT JOBS...")
+  (let [content (read-json-content)]
+    (println "teste: " content))
+    (recur :continue))
