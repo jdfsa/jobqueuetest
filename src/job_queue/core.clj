154a87c37032d60e6a7527c40b9e4528b88fea98
jacques.sa@outlook.com <jacques.sa@outlook.com>
2018-06-27 21:32:50 -0300

    push agents and jobs to repository

--- src/job_queue/core.clj
+++ src/job_queue/core.clj
@@ -4,12 +4,28 @@
 (defn read-json-content []
   (json/read (java.io.BufferedReader. (java.io.InputStreamReader. System/in))))
 
+(def agents-repository (ref {}))
+(def jobs-repository (ref {}))
+
+(defn push [item repository]
+  (dosync (alter repository conj (deref repository) item)))
+
 (defn process-content
   [job-data]
+  
   (loop [job-data job-data]
-    (when-not (empty? job-data)
-      (println (first job-data))
-      (recur (rest job-data)))))
+    (let [first-item (first job-data)]      
+      (when-not (empty? job-data)
+        
+        (def new-agent (first-item "new_agent"))
+        (def new-job (first-item "new_job"))
+
+        (println "debug: " (first-item "new_job"))
+        
+        (when-not (nil? new-agent) (push new-agent agents-repository) :new-agent)
+        (when-not (nil? new-job) (push new-job jobs-repository) :new-job)
+        
+        (recur (rest job-data))))))
 
 (defn -main [& args]
   (println "\n\nIMPORT JOBS...")
