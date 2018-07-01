182dd0c8428340522b4c7a749086599f963cf163
jacques.sa@outlook.com <jacques.sa@outlook.com>
2018-07-01 00:51:57 -0300

    implementing basic tests

--- test/job_queue/core_test.clj
+++ test/job_queue/core_test.clj
@@ -1,7 +1,22 @@
 (ns job-queue.core-test
   (:require [clojure.test :refer :all]
+            [cheshire.core :as cheshire]
+            [clojure.java.io :as io]
             [job-queue.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn read-json [filename]
+  (cheshire/parse-stream
+    (clojure.java.io/reader (io/resource filename))
+    (fn [k] (keyword k))))
+
+(deftest test-input-1
+  (testing "Should pass test input 1"
+    (let [actual (process-content (read-json "expected-input-1.json"))
+          expected (read-json "expected-output-1.json")]
+      (is (= actual expected)))))
+
+(deftest test-input-2
+  (testing "Should pass test input 2"
+    (let [actual (process-content (read-json "expected-input-2.json"))
+          expected (read-json "expected-output-2.json")]
+      (is (= actual expected)))))
