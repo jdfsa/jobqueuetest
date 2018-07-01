b418e210cfb01fa0bbd50077869b6502539903ad
jacques.sa@outlook.com <jacques.sa@outlook.com>
2018-07-01 01:50:00 -0300

    more tests

--- test/job_queue/core_test.clj
+++ test/job_queue/core_test.clj
@@ -2,21 +2,62 @@
   (:require [clojure.test :refer :all]
             [cheshire.core :as cheshire]
             [clojure.java.io :as io]
-            [job-queue.core :refer :all]))
+            [job-queue.core :as core]))
 
 (defn read-json [filename]
   (cheshire/parse-stream
     (clojure.java.io/reader (io/resource filename))
     (fn [k] (keyword k))))
 
+(deftest get-job-request
+  (testing "Should format a job request based on agent and job ids"
+    (let [expected { :job_assigned { :job_id "job-test-9001" :agent_id "agent-test-0001" }}]
+      (is (= (core/get-job-request "agent-test-0001" "job-test-9001") expected)))))
+
+(deftest get-agent-by-id
+  (testing "Should return an agent by a specific id"
+    (let [input [
+                  { 
+                    :id "8ab86c18-3fae-4804-bfd9-c3d6e8f66260",
+                    :name "BoJack Horseman",
+                    :primary_skillset ["bills-questions"],
+                    :secondary_skillset []
+                  }
+                  { 
+                    :id "ed0e23ef-6c2b-430c-9b90-cd4f1ff74c88",
+                    :name "Mr. Peanut Butter",
+                    :primary_skillset ["rewards-question"],
+                    :secondary_skillset ["bills-questions"]
+                  }
+              ]
+          expected {
+            :id "ed0e23ef-6c2b-430c-9b90-cd4f1ff74c88",
+            :name "Mr. Peanut Butter",
+            :primary_skillset ["rewards-question"],
+            :secondary_skillset ["bills-questions"]
+          }]
+      (is (= (core/get-agent-by-id input "ed0e23ef-6c2b-430c-9b90-cd4f1ff74c88") expected)))))
+
 (deftest test-input-1
   (testing "Should pass test input 1"
-    (let [actual (process-content (read-json "expected-input-1.json"))
+    (let [actual (core/process-content (read-json "expected-input-1.json"))
           expected (read-json "expected-output-1.json")]
       (is (= actual expected)))))
 
 (deftest test-input-2
   (testing "Should pass test input 2"
-    (let [actual (process-content (read-json "expected-input-2.json"))
+    (let [actual (core/process-content (read-json "expected-input-2.json"))
           expected (read-json "expected-output-2.json")]
+      (is (= actual expected)))))
+
+(deftest test-input-3
+  (testing "Should pass test input 3"
+    (let [actual (core/process-content (read-json "expected-input-3.json"))
+          expected (read-json "expected-output-3.json")]
+      (is (= actual expected)))))
+
+(deftest test-input-4
+  (testing "Should pass test input 4"
+    (let [actual (core/process-content (read-json "expected-input-4.json"))
+          expected (read-json "expected-output-4.json")]
       (is (= actual expected)))))
